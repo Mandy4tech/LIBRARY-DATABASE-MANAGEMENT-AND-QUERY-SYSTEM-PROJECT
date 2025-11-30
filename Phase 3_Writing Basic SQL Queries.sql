af73e598812a05c1d1dd57db84cd67c263697424
Mandy4tech <flamersgem28@gmail.com>
2025-11-30 12:46:36 +0100

    Add files via upload

---
+++ Phase 3_Writing Basic SQL Queries.sql
@@ -0,0 +1,22 @@
+use library_db; 
+
+/*
+View all books
+Show all registered members
+Find all loans made on a specific date
+Display all books with more than 3 available copies
+*/
+
+-- View all books
+select *
+from books;
+
+-- Show all registered members
+select *
+from members;
+
+-- Find all loans made on a specific date
+select *
+from loans;
+
+-- Display all books with more than 3 available copies
